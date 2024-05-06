4d37aaaebcae937f3978520f23b75a5e4c391a2f
Lautaro Scuffi <scuffilautaro@gmail.com>
2024-05-06 14:06:09 -0300

    cambia nombre paths

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package TPEProg3;
 
 public class Main {
     public static void main(String[] args) {
-        Servicios s1 = new Servicios("TPE/datasets/Procesadores.csv", "TPE/datasets/Tareas.csv");
+        Servicios s1 = new Servicios("TPEProg3/datasets/Procesadores.csv", "TPEProg3/datasets/Tareas.csv");
 
         System.out.println(s1.servicio3(70, 80));
     }
